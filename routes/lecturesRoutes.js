cda454601de8ca0a06f6d5303bec2e8d7302ae95
AgentBraun <AgentBraun@users.noreply.github.com>
2023-05-01 02:03:44 +0200

    post request for new lectures fails, need fixing

--- routes/lecturesRoutes.js
+++ routes/lecturesRoutes.js
@@ -23,4 +23,23 @@ router.route('/predavanja').get((req, res) => {
   });
 });
 
+router.route('/predavanja/dodajPredavanja').post((req, res) => {
+  console.log('post request dodaj predavanje');
+  const q =
+    'INSERT INTO lecture (`title`, `date`, `duration`,`scaning`, `qrCode`, `subjectID`) VALUES (?)';
+  const values = [
+    req.body.title,
+    req.body.date,
+    req.body.duration,
+    req.body.scaning,
+    req.body.qrCode,
+    req.body.subjectID,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    else res.json('lecture has been created successfuly');
+  });
+});
+
 module.exports = router;
